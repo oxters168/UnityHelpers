31cf72703f8a4c6377b69a5bd3dfbcef92e70226
Oxters Wyzgowski <oxters168@gmail.com>
2019-06-28 03:49:27 +0300

    Created a pool manager.

---
+++ ObjectPool/PoolInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PoolInfo
+{
+    public string poolName;
+    public Transform poolPrefab;
+    public int poolSize = 5;
+    public bool reuseObjectsInUse = true;
+    public Transform poolParent;
+    public bool worldPositionStays = true;
+}
---
+++ ObjectPool/PoolManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class PoolManager : MonoBehaviour
+{
+    private static PoolManager poolManagerInScene;
+    public PoolInfo[] pools;
+    private Dictionary<string, ObjectPool<Transform>> storedPools = new Dictionary<string, ObjectPool<Transform>>();
+
+    private void Awake()
+    {
+        poolManagerInScene = this;
+        foreach(PoolInfo pool in pools)
+        {
+            bool doesNotLearn = !storedPools.ContainsKey(pool.poolName);
+            Debug.Assert(doesNotLearn);
+            if (doesNotLearn)
+                storedPools.Add(pool.poolName, new ObjectPool<Transform>(pool.poolPrefab, pool.poolSize, pool.reuseObjectsInUse, pool.poolParent, pool.worldPositionStays));
+        }
+    }
+
+    public static ObjectPool<Transform> GetPool(string poolName)
+    {
+        bool hasKey = poolManagerInScene.storedPools.ContainsKey(poolName);
+        Debug.Assert(hasKey);
+        return hasKey ? poolManagerInScene.storedPools[poolName] : null;
+    }
+}
