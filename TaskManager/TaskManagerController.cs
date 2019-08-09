042e886426296ea07f59e33d70832537340babb6
Oxters Wyzgowski <oxters168@gmail.com>
2019-08-09 23:52:01 +0300

    Made TaskManagerController require to be placed in the scene rather than created when called to. Changed how tasks are called in TaskManagerController to be one from each type each frame.

--- TaskManager/TaskManagerController.cs
+++ TaskManager/TaskManagerController.cs
@@ -15,27 +15,36 @@ namespace UnityHelpers
         private List<(string, Func<Task>)> funkyTasks = new List<(string, Func<Task>)>();
         private List<Action> actions = new List<Action>();
 
+        private void Awake()
+        {
+            if (!taskManagerControllerInScene)
+                taskManagerControllerInScene = this;
+        }
         async void Update()
         {
-            for (int i = asyncActions.Count - 1; i >= 0; i--)
+            if (asyncActions.Count > 0)
             {
+                int i = asyncActions.Count - 1;
                 string taskName = asyncActions[i].Item1;
                 Action action = asyncActions[i].Item2;
                 asyncActions.RemoveAt(i);
                 await TaskWrapper.CreateTask(action, taskName, (task) => { SteamController.LogToConsole("Running task " + taskName); tasks.Add(task); }, (task) => { SteamController.LogToConsole("Completed task " + taskName); tasks.Remove(task); });
             }
-            for (int i = funkyTasks.Count - 1; i >= 0; i--)
+            if (funkyTasks.Count > 0)
             {
+                int i = funkyTasks.Count - 1;
                 string taskName = funkyTasks[i].Item1;
                 Func<Task> action = funkyTasks[i].Item2;
                 funkyTasks.RemoveAt(i);
                 await TaskWrapper.CreateTask(action, taskName, (task) => { SteamController.LogToConsole("Running task " + taskName); tasks.Add(task); }, (task) => { SteamController.LogToConsole("Completed task " + taskName); tasks.Remove(task); });
             }
-            for (int i = actions.Count - 1; i >= 0; i--)
+            if (actions.Count > 0)
             {
+                int i = actions.Count - 1;
                 Action action = actions[i];
                 actions.RemoveAt(i);
-                action();
+                Debug.Assert(action != null);
+                action?.Invoke();
             }
         }
 
@@ -44,7 +53,7 @@ namespace UnityHelpers
             if (action == null)
                 throw new ArgumentNullException("Action cannot be null");
 
-            GetSelfInScene().actions.Insert(0, action);
+            taskManagerControllerInScene.actions.Insert(0, action);
         }
         public static void RunActionAsync(string name, Action action)
         {
@@ -55,7 +64,7 @@ namespace UnityHelpers
             if (HasTask(name))
                 throw new InvalidOperationException("A task already exists with the name " + name);
 
-            GetSelfInScene().asyncActions.Insert(0, (name, action));
+            taskManagerControllerInScene.asyncActions.Insert(0, (name, action));
         }
         public static void RunActionAsync(string name, Func<Task> action)
         {
@@ -66,14 +75,14 @@ namespace UnityHelpers
             if (HasTask(name))
                 throw new InvalidOperationException("A task already exists with the name " + name);
 
-            GetSelfInScene().funkyTasks.Insert(0, (name, action));
+            taskManagerControllerInScene.funkyTasks.Insert(0, (name, action));
         }
         public static void CancelTask(string name)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("Name cannot be empty or null");
 
-            var tasks = GetSelfInScene().tasks;
+            var tasks = taskManagerControllerInScene.tasks;
             TaskWrapper task = tasks.FirstOrDefault(checkedTask => checkedTask.name.Equals(name, StringComparison.Ordinal));
             if (task == null)
                 throw new KeyNotFoundException("Could not find task with name " + name);
@@ -83,7 +92,7 @@ namespace UnityHelpers
         }
         public static bool HasTask(string name)
         {
-            var self = GetSelfInScene();
+            var self = taskManagerControllerInScene;
             bool contains = false;
             if (self.asyncActions.Exists(item => item.Item1.Equals(name, StringComparison.Ordinal)))
                 contains = true;
@@ -92,13 +101,6 @@ namespace UnityHelpers
             else if (self.tasks.Exists(task => task.name.Equals(name, StringComparison.Ordinal)))
                 contains = true;
             return contains;
-        }
-
-        private static TaskManagerController GetSelfInScene()
-        {
-            if (!taskManagerControllerInScene)
-                taskManagerControllerInScene = new GameObject("TaskManager").AddComponent<TaskManagerController>();
-            return taskManagerControllerInScene;
         }
     }
 }
