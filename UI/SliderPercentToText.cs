9d01254f36f0d68138101bfe5c5c4a29b65f8b25
Oxters Wyzgowski <oxters168@gmail.com>
2019-10-28 17:44:35 +0200

    Added a UI helper that updates a TMP label to a slider's percent.

---
+++ UI/SliderPercentToText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class SliderPercentToText : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI label;
+    public string prefix;
+    public string postfix;
+
+    private void Start()
+    {
+        var slider = GetComponent<Slider>();
+        if (slider != null)
+            slider.onValueChanged.AddListener(UpdateText);
+        else
+            Debug.LogError("Script must be attached to a slider");
+
+        UpdateText(slider.value);
+    }
+
+    private void UpdateText(float value)
+    {
+        if (label != null)
+        {
+            int percentValue = Mathf.RoundToInt(value * 100);
+            label.text = prefix + percentValue + postfix;
+        }
+    }
+}
