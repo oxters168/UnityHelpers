25f9216bafdd72732f4c6ef071cd02200d64819c
Oxters Wyzgowski <oxters168@gmail.com>
2019-10-30 20:11:33 +0200

    Moved TouchGesturesHandler to UI folder. Added MimicTransform component.

---
+++ MimicTransform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UnityHelpers
+{
+    public class MimicTransform : MonoBehaviour
+    {
+        public Transform other;
+        private bool errored;
+
+        void LateUpdate()
+        {
+            if (other != null)
+            {
+                errored = false;
+                transform.position = other.position;
+                transform.rotation = other.rotation;
+            }
+            else if (!errored)
+            {
+                Debug.LogError("Can't mimic nothing");
+                errored = true;
+            }
+        }
+    }
+}
