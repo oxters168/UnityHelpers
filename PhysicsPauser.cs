5e46a0059740a7738e1cd6e31690fe7d203b8fbd
Oxters Wyzgowski <oxters168@gmail.com>
2019-11-15 23:22:53 +0200

    Added RailSystemController. Added PhysicsPauser.

---
+++ PhysicsPauser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace UnityHelpers
+{
+    /// <summary>
+    /// Use this to pause and unpause all non kinematic rigidbodies in your scene
+    /// </summary>
+    public static class PhysicsPauser
+    {
+        private static Dictionary<Rigidbody, (Vector3, Vector3)> history = new Dictionary<Rigidbody, (Vector3, Vector3)>();
+
+        public static void Pause(bool onOff)
+        {
+            if (onOff)
+            {
+                foreach (var physicsObject in Object.FindObjectsOfType<Rigidbody>())
+                {
+                    if (!physicsObject.isKinematic) //if something was originally kinematic don't touch it
+                    {
+                        history[physicsObject] = (physicsObject.velocity, physicsObject.angularVelocity);
+                        physicsObject.isKinematic = true;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var historyObject in history)
+                {
+                    historyObject.Key.isKinematic = false;
+                    historyObject.Key.velocity = historyObject.Value.Item1;
+                    historyObject.Key.angularVelocity = historyObject.Value.Item2;
+                }
+                history.Clear();
+            }
+        }
+    }
+}
---
+++ RailSystemController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace UnityHelpers
+{
+    /// <summary>
+    /// Moves a target along rail checkpoints
+    /// </summary>
+    public class RailSystemController : MonoBehaviour
+    {
+        public Transform target;
+        public float targetLerp = 5;
+        public bool play;
+        [Range(0, 1)]
+        public float value;
+        [Range(-1, 1)]
+        public float speed = 0.01f;
+        public bool loop;
+
+        public bool showCheckpointsGizmos;
+        public PosRotWrapper[] checkpoints;
+
+        void Update()
+        {
+            if (target != null && checkpoints?.Length > 0)
+            {
+                if (loop && value >= 1)
+                    value = 0;
+
+                float totalLength = checkpoints.Length - (loop ? 0 : 1);
+
+                int index = Mathf.FloorToInt(value * totalLength);
+                int nextIndex = (index + (speed > 0 ? 1 : -1) + checkpoints.Length) % checkpoints.Length;
+                float lerp = (value * totalLength) - index;
+
+                target.position = Vector3.Lerp(target.position, Vector3.Lerp(checkpoints[index].position, checkpoints[nextIndex].position, checkpoints[index].transition.Evaluate(lerp)), Time.deltaTime * targetLerp);
+                target.rotation = Quaternion.Lerp(target.rotation, Quaternion.Lerp(Quaternion.Euler(checkpoints[index].rotation), Quaternion.Euler(checkpoints[nextIndex].rotation), checkpoints[index].transition.Evaluate(lerp)), Time.deltaTime * targetLerp);
+
+                if (play)
+                    value = Mathf.Clamp01(value + speed);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (showCheckpointsGizmos)
+            {
+                Gizmos.color = Color.green;
+                if (checkpoints != null)
+                    for (int i = 0; i < checkpoints.Length; i++)
+                    {
+                        Gizmos.matrix = Matrix4x4.TRS(checkpoints[i].position, Quaternion.Euler(checkpoints[i].rotation), Vector3.one);
+                        Gizmos.DrawFrustum(Vector3.zero, Camera.main.fieldOfView, 1, 0.01f, Camera.main.aspect);
+                    }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct PosRotWrapper
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+        public AnimationCurve transition;
+    }
+}
