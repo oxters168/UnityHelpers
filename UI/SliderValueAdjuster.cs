2062170e6b6e5a5955bdf6279319a2f6b8e7c1db
Oxters Wyzgowski <oxters168@gmail.com>
2019-12-15 22:24:36 +0200

    Changed the way SliderValueToText works to be through the update function. Added decimal formatting to SliderValueToText. Added an error that gets logged when slider or label not found in SliderValueToText. Created SliderValueAdjuster, currently with only decimal place adjustment.

---
+++ UI/SliderValueAdjuster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityHelpers
+{
+    [ExecuteAlways]
+    public class SliderValueAdjuster : MonoBehaviour
+    {
+        private Slider slider;
+        private bool sliderErrored;
+
+        [Range(0, 10)]
+        public uint decimalPlaces;
+
+        void Update()
+        {
+            if (slider == null)
+                slider = GetComponent<Slider>();
+
+            if (slider != null)
+            {
+                sliderErrored = false;
+                slider.value = MathHelpers.SetDecimalPlaces(slider.value, decimalPlaces);
+            }
+            else if (!sliderErrored)
+            {
+                Debug.LogError("SliderValueAdjuster(" + transform.name + "): Could not find slider component, this script should be attached to the same object that has the slider component");
+                sliderErrored = true;
+            }
+        }
+    }
+}
--- UI/SliderValueToText.cs
+++ UI/SliderValueToText.cs
@@ -6,31 +6,62 @@ namespace UnityHelpers
     [ExecuteAlways]
     public class SliderValueToText : MonoBehaviour
     {
-        public bool percent;
+        private Slider slider;
+        private bool sliderErrored, labelErrored;
 
-        public TMPro.TextMeshProUGUI label;
+        public TMPro.TextMeshProUGUI targetLabel;
+
+        [Space(10)]
+
         public string prefix;
         public string postfix;
 
-        private void Start()
-        {
-            var slider = GetComponent<Slider>();
-            if (slider != null)
-                slider.onValueChanged.AddListener(UpdateText);
-            else
-                Debug.LogError("Script must be attached to a slider");
+        [Space(10)]
 
-            UpdateText(slider.value);
+        public bool percent;
+
+        [Space(10)]
+
+        public bool formatDecimalPlaces;
+        [Range(0, 99)]
+        public uint decimalPlaces = 2;
+
+        private void Update()
+        {
+            if (slider == null)
+                slider = GetComponent<Slider>();
+
+            if (slider != null)
+            {
+                sliderErrored = false;
+                UpdateText(slider.value);
+            }
+            else if (!sliderErrored)
+            {
+                Debug.LogError("SliderValueToText(" + transform.name + "): Could not find slider component, this script should be attached to the same object that has the slider component");
+                sliderErrored = true;
+            }
         }
 
         private void UpdateText(float value)
         {
-            if (label != null)
+            if (targetLabel != null)
             {
+                labelErrored = false;
+
                 float shownValue = value;
                 if (percent)
                     shownValue = Mathf.RoundToInt(shownValue * 100);
-                label.text = prefix + shownValue + postfix;
+
+                string valueString = shownValue.ToString();
+                if (formatDecimalPlaces)
+                    valueString = string.Format("{0:F" + decimalPlaces + "}", shownValue);
+                targetLabel.text = prefix + valueString + postfix;
+            }
+            else if (!labelErrored)
+            {
+                Debug.LogError("SliderValueToText(" + transform.name + "): Missing target label");
+                labelErrored = true;
             }
         }
     }
