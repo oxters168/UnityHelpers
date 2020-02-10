6d24777829546b2f4f80557acf62f506e38a7521
Oxters Wyzgowski <oxters168@gmail.com>
2020-02-10 22:42:50 +0200

    Seems I forgot to put HealthController in the UnityHelpers namespace. Changed value to not have private set property so it shows up in inspector.

--- HealthController.cs
+++ HealthController.cs
@@ -1,73 +1,77 @@
 ﻿using UnityEngine;
 
-public class HealthController : MonoBehaviour
+namespace UnityHelpers
 {
-    [Range(0, 1)]
-    public float startPercent = 1;
-
-    public float value { get; private set; }
-    public float fullValue = 100;
-
-    public HealthEvent onValueChanged, onHurt, onHealed, onDead;
-
-    private void Start()
+    public class HealthController : MonoBehaviour
     {
-        SetPercent(startPercent);
-    }
+        [Range(0, 1)]
+        public float startPercent = 1;
+        public float fullValue = 100;
 
-    public void HealPercent(float percent)
-    {
-        Add(percent);
-    }
-    public void HealValue(float value)
-    {
-        value = Mathf.Clamp(Mathf.Abs(value), 0, float.MaxValue);
-        Add(value / fullValue);
-    }
-    public void HurtPercent(float percent)
-    {
-        Remove(percent);
-    }
-    public void HurtValue(float value)
-    {
-        value = Mathf.Clamp(Mathf.Abs(value), 0, float.MaxValue);
-        Remove(value / fullValue);
-    }
+        [Space(10), Tooltip("The current health value, do not modify directly")]
+        public float value;
 
-    public void SetPercent(float percent)
-    {
-        float delta = percent - value;
-        if (delta < 0)
-            Remove(delta);
-        else if (delta > 0)
-            Add(delta);
-    }
-    public void SetValue(float value)
-    {
-        float percent = value / fullValue;
-        SetPercent(percent);
-    }
+        public HealthEvent onValueChanged, onHurt, onHealed, onDead;
 
-    private void Add(float percent)
-    {
-        percent = Mathf.Clamp01(Mathf.Abs(percent));
-        value = Mathf.Clamp01(value + percent);
+        private void Start()
+        {
+            SetPercent(startPercent);
+        }
 
-        onValueChanged?.Invoke(value);
-        onHealed?.Invoke(percent);
+        public void HealPercent(float percent)
+        {
+            Add(percent);
+        }
+        public void HealValue(float value)
+        {
+            value = Mathf.Clamp(Mathf.Abs(value), 0, float.MaxValue);
+            Add(value / fullValue);
+        }
+        public void HurtPercent(float percent)
+        {
+            Remove(percent);
+        }
+        public void HurtValue(float value)
+        {
+            value = Mathf.Clamp(Mathf.Abs(value), 0, float.MaxValue);
+            Remove(value / fullValue);
+        }
+
+        public void SetPercent(float percent)
+        {
+            float delta = percent - value;
+            if (delta < 0)
+                Remove(delta);
+            else if (delta > 0)
+                Add(delta);
+        }
+        public void SetValue(float value)
+        {
+            float percent = value / fullValue;
+            SetPercent(percent);
+        }
+
+        private void Add(float percent)
+        {
+            percent = Mathf.Clamp01(Mathf.Abs(percent));
+            value = Mathf.Clamp01(value + percent);
+
+            onValueChanged?.Invoke(value);
+            onHealed?.Invoke(percent);
+        }
+        private void Remove(float percent)
+        {
+            percent = Mathf.Clamp01(Mathf.Abs(percent));
+            value = Mathf.Clamp01(value - percent);
+
+            onValueChanged?.Invoke(value);
+            onHurt?.Invoke(-percent);
+            if (value <= 0)
+                onDead?.Invoke(-percent);
+        }
+
+        [System.Serializable]
+        public class HealthEvent : UnityEngine.Events.UnityEvent<float>
+        { }
     }
-    private void Remove(float percent)
-    {
-        percent = Mathf.Clamp01(Mathf.Abs(percent));
-        value = Mathf.Clamp01(value - percent);
-
-        onValueChanged?.Invoke(value);
-        onHurt?.Invoke(-percent);
-        if (value <= 0)
-            onDead?.Invoke(-percent);
-    }
-
-    [System.Serializable]
-    public class HealthEvent : UnityEngine.Events.UnityEvent<float>
-    { }
 }
