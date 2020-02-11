ce79f1c19425685ee85d9420e99326b1b1681a58
Oxters Wyzgowski <oxters168@gmail.com>
2020-02-12 00:16:42 +0200

    Added ability to not mimic an axis in MimicTransform. Added ability to look at object rather than mimic rotation in MimicTransform. Changed order of quaternion multiplication for rotation offset in MimicTransform.

--- MimicTransform.cs
+++ MimicTransform.cs
@@ -5,12 +5,16 @@ namespace UnityHelpers
     public class MimicTransform : MonoBehaviour
     {
         public Transform other;
+        public Transform lookAt;
+        public bool mimicX = true, mimicY = true, mimicZ = true;
         [Space(10)]
         public Vector3 offset;
         public Vector3 rotOffset;
         [Space(10)]
-        public bool lerp;
-        public float lerpAmount = 5;
+        public bool lerpPosition;
+        public float lerpPositionAmount = 5;
+        public bool lerpRotation;
+        public float lerpRotationAmount = 5;
         private bool errored;
 
         void Update()
@@ -19,16 +23,18 @@ namespace UnityHelpers
             {
                 errored = false;
 
-                Vector3 nextPosition = other.position + other.right * offset.x + other.up * offset.y + other.forward * offset.z;
-                Quaternion nextRotation = Quaternion.Euler(rotOffset) * other.rotation;
-
-                if (lerp)
-                {
-                    nextPosition = Vector3.Lerp(transform.position, nextPosition, Time.deltaTime * lerpAmount);
-                    nextRotation = Quaternion.Lerp(transform.rotation, nextRotation, Time.deltaTime * lerpAmount);
-                }
-
+                Vector3 mimickedPosition = new Vector3(mimicX ? other.position.x : transform.position.x, mimicY ? other.position.y : transform.position.y, mimicZ ? other.position.z : transform.position.z);
+                Vector3 nextPosition = mimickedPosition + other.right * offset.x + other.up * offset.y + other.forward * offset.z;
+                if (lerpPosition)
+                    nextPosition = Vector3.Lerp(transform.position, nextPosition, Time.deltaTime * lerpPositionAmount);
                 transform.position = nextPosition;
+
+                Quaternion mimickedRotation = other.rotation;
+                if (lookAt != null)
+                    mimickedRotation = Quaternion.LookRotation(lookAt.position - transform.position);
+                Quaternion nextRotation = mimickedRotation * Quaternion.Euler(rotOffset);
+                if (lerpRotation)
+                    nextRotation = Quaternion.Lerp(transform.rotation, nextRotation, Time.deltaTime * lerpRotationAmount);
                 transform.rotation = nextRotation;
             }
             else if (!errored)
