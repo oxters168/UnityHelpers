9775c2f5fa04d6e903d2c6f905f1df7924e4b055
Oxters Wyzgowski <oxters168@gmail.com>
2020-02-24 02:31:55 +0200

    Made a physics transform that moves an object to a position using AddForce. For now object moves slow and only position is working.

---
+++ Physics/PhysicsTransform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UnityHelpers
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PhysicsTransform : MonoBehaviour
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public float strength = 1;
+        public bool strive;
+        private Rigidbody affectedBody;
+
+        void Awake()
+        {
+            affectedBody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            if (strive)
+            {
+                Vector3 direction = position - transform.position;
+                Vector3 deltaVelocity = direction - affectedBody.velocity;
+                if (affectedBody.useGravity && !affectedBody.isKinematic)
+                    affectedBody.AddForce(-Physics.gravity * affectedBody.mass);
+                    //deltaVelocity += Physics.gravity * Time.fixedDeltaTime;
+                affectedBody.AddForce(deltaVelocity * strength, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
