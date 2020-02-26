3899cef2ad8d28bdd0d1e1c6fc570818655ba7a8
Oxters Wyzgowski <oxters168@gmail.com>
2020-02-26 02:38:14 +0200

    Added way to "parent" a PhysicsTransform to a Transform.

--- Physics/PhysicsTransform.cs
+++ Physics/PhysicsTransform.cs
@@ -5,16 +5,38 @@ namespace UnityHelpers
     [RequireComponent(typeof(Rigidbody))]
     public class PhysicsTransform : MonoBehaviour
     {
+        public Transform anchor;
+
         public bool striveForPosition = true;
         public bool striveForOrientation = true;
         public bool counteractGravity = true; //Suzan told me about PID controllers and how they work, so maybe in the future I can add the I to positional strivingness to counteract gravity/friction automatically.
         
         [Space(10)]
         public Vector3 position;
+        public Vector3 localPosition
+        {
+            set
+            {
+                if (anchor != null)
+                    position = anchor.TransformPoint(value);
+                else
+                    position = value;
+            }
+        }
         public float strength = 10;
         public float maxSpeed = float.MaxValue;
         [Space(10)]
         public Quaternion rotation = Quaternion.identity;
+        public Quaternion localRotation
+        {
+            set
+            {
+                if (anchor != null)
+                    rotation = anchor.TransformRotation(value);
+                else
+                    rotation = value;
+            }
+        }
         [Tooltip("Frequency is the speed of convergence. If damping is 1, frequency is the 1/time taken to reach ~95% of the target value. i.e. a frequency of 6 will bring you very close to your target within 1/6 seconds.")]
         public float frequency = 6;
         [Tooltip("damping = 1, the system is critically damped\ndamping is greater than 1 the system is over damped(sluggish)\ndamping is less than 1 the system is under damped(it will oscillate a little)")]
