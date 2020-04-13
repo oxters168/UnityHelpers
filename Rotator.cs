9c902c757e94f5ba6ef924d99608932cd0af94a8
Oxters Wyzgowski <oxters168@gmail.com>
2020-04-13 19:50:17 +0300

    Made the angle modifiable in Rotator. Added an option in Rotator to visualize the spin axis on a given Transform.

--- Rotator.cs
+++ Rotator.cs
@@ -4,17 +4,26 @@ namespace UnityHelpers
 {
     public class Rotator : MonoBehaviour
     {
-        [Tooltip("In rotations/second")]
+        [Tooltip("The angle to be set on the specified axis")]
+        public float angle;
+        [Tooltip("The speed of change of the angle in revolutions/second")]
         public float rotSpeed;
 
         [Tooltip("The local axis to spin on")]
         public Vector3 spinAxis = Vector3.up;
+        [Tooltip("A transform that will have it's forward set to spinAxis")]
+        public Transform applyAxisOnTransform;
 
         private void Update()
         {
-            float degrees = rotSpeed * 360f * Time.deltaTime;
-            Quaternion quatRot = Quaternion.AngleAxis(degrees, spinAxis.normalized);
-            transform.rotation = transform.rotation * quatRot;
+            spinAxis = spinAxis.normalized;
+
+            if (applyAxisOnTransform != null)
+                applyAxisOnTransform.forward = spinAxis;
+
+            angle += rotSpeed * 360f * Time.deltaTime;
+            Quaternion quatRot = Quaternion.AngleAxis(angle, spinAxis);
+            transform.rotation = quatRot;
         }
 
         private void OnDrawGizmosSelected()
