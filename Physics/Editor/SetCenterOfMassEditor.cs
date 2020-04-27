107f22077bcc0978c767a9619a4727bd94d5cdb3
Oxters Wyzgowski <oxters168@gmail.com>
2020-04-27 18:12:48 +0300

    Fixed an issue with SetCenterOfMass where the COM value would change on run. Changed the scene COM position handle to be done through editor script rather than an attribute. SetCenterOfMass is now capable of being multi-object editted.

---
+++ Physics/Editor/SetCenterOfMassEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityHelpers
+{
+    [CustomEditor(typeof(SetCenterOfMass)), CanEditMultipleObjects]
+    public class SetCenterOfMassEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+        void OnSceneGUI()
+        {
+            var currentSelf = target as SetCenterOfMass;
+
+            EditorGUI.BeginChangeCheck();
+            var newCenterOfMass = currentSelf.transform.InverseTransformPoint(Handles.PositionHandle(currentSelf.transform.TransformPoint(currentSelf.centerOfMass), Quaternion.identity));
+            newCenterOfMass = newCenterOfMass.SetDecimalPlaces(3);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Changed center of mass");
+                currentSelf.centerOfMass = newCenterOfMass;
+            }
+        }
+    }
+}
