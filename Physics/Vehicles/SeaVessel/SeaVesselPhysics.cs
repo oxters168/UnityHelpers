13a87942112216d4256f645de4ab4c51162e380d
Oxters <oxters168@gmail.com>
2020-11-15 16:30:59 +0200

    Created a new vehicle script for sea vessels.

---
+++ Physics/Vehicles/SeaVessel/SeaVesselPhysics.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+
+namespace UnityHelpers
+{
+    public class SeaVesselPhysics : MonoBehaviour, IValueManager
+    {
+        public Rigidbody vesselBody;
+
+        [Space(10), Tooltip("The maximum speed the ship can reach (meters per second)")]
+        public float maxSpeed = 5;
+        [Tooltip("How fast to reach the max speed (meters per second squared)")]
+        public float acceleration = 0.3f;
+        [Tooltip("The maximum force that can be applied to the ship to achieve its strived speed")]
+        public float maxForce = 120f;
+        [Tooltip("How fast to reach the max rotation speed (degrees per second squared)")]
+        public float rotAcceleration = 0.01f;
+        [Tooltip("The maximum speed the ship can rotate (degrees per second)")]
+        public float maxRotSpeed = 0.5f;
+        [Tooltip("The maximum torque that can be applied to the ship to achieve its strived orientation")]
+        public float maxTorque = 120f;
+        private float currentRotSpeed = 0;
+
+        private Vector2 dpad;
+        private bool crossBtn;
+
+        [Space(10)]
+        public ValuesVault values;
+
+        void Update()
+        {
+            //Retrieve input
+            dpad = new Vector2(GetAxis("dpadHor"), GetAxis("dpadVer"));
+            crossBtn = GetToggle("crossBtn");
+        }
+        void FixedUpdate()
+        {
+            if (crossBtn)
+            {
+                Vector3 planarForward = vesselBody.transform.forward.Planar(Vector3.up);
+                Vector3 currentVelocity = vesselBody.velocity.Planar(Vector3.up);
+                float currentSpeed = currentVelocity.magnitude * Vector3.Dot(currentVelocity.normalized, planarForward);
+                currentSpeed = Mathf.Clamp(currentSpeed + acceleration, -maxSpeed, maxSpeed);
+                Vector3 pushForce = vesselBody.CalculateRequiredForceForSpeed(currentSpeed * planarForward, Time.fixedDeltaTime, false, maxForce);
+                Debug.DrawRay(vesselBody.position + Vector3.up * 20, planarForward * 10, Color.blue);
+                Debug.DrawRay(vesselBody.position + Vector3.up * 20, currentVelocity, Color.green);
+                Debug.Log(currentSpeed);
+                // Debug.Log(pushForce.magnitude);
+                vesselBody.AddForce(pushForce, ForceMode.Force);
+            }
+
+
+            Vector3 torque = Vector3.zero;
+            if (Mathf.Abs(dpad.x) > float.Epsilon || Mathf.Abs(dpad.y) > float.Epsilon)
+            {
+                float requestedAngle = Vector2.SignedAngle(dpad.ToCircle(), Vector2.up);
+
+                float currentAngle = Vector2.SignedAngle(vesselBody.transform.forward.Planar(Vector3.up).xz(), Vector2.up);
+                Quaternion currentUpOrientation = Quaternion.AngleAxis(currentAngle, Vector3.up);
+                Quaternion requestedUpOrientation = Quaternion.AngleAxis(requestedAngle, Vector3.up);
+                Quaternion orientationDiff = requestedUpOrientation * Quaternion.Inverse(currentUpOrientation);
+                orientationDiff = orientationDiff.Shorten();
+                float angleDiff;
+                Vector3 axis;
+                orientationDiff.ToAngleAxis(out angleDiff, out axis);
+
+                // float expectedAcc = angleDiff / (Time.fixedDeltaTime * Time.fixedDeltaTime);
+                float rotDirection = Mathf.Sign(Vector3.Dot(Vector3.up, axis));
+                if (Mathf.Abs(currentRotSpeed) > angleDiff)
+                    currentRotSpeed = angleDiff;
+                // if (rotAcceleration > expectedAcc)
+                //     currentRotSpeed = Mathf.Clamp(currentRotSpeed - rotAcceleration * rotDirection, -maxRotSpeed, maxRotSpeed);
+                else
+                    currentRotSpeed = Mathf.Clamp(currentRotSpeed + rotAcceleration * rotDirection, -maxRotSpeed, maxRotSpeed);
+            }
+            else if (Mathf.Abs(currentRotSpeed) > float.Epsilon)
+            {
+                float rotDirection = Mathf.Sign(currentRotSpeed);
+                if (Mathf.Abs(currentRotSpeed) > rotAcceleration)
+                    currentRotSpeed += rotAcceleration * -rotDirection;
+                else
+                    currentRotSpeed = 0;
+            }
+
+            if (Mathf.Abs(currentRotSpeed) > float.Epsilon)
+            {
+                Quaternion shipOrientation = vesselBody.rotation * Quaternion.AngleAxis(currentRotSpeed, Vector3.up);
+                torque = vesselBody.CalculateRequiredTorqueForRotation(shipOrientation, Time.fixedDeltaTime, maxTorque);
+                // Debug.Log(torque.magnitude);
+                vesselBody.AddTorque(torque, ForceMode.Force);
+            }
+        }
+
+        public void SetAxis(string name, float value)
+        {
+            values.GetValue(name).SetAxis(value);
+        }
+        public float GetAxis(string name)
+        {
+            return values.GetValue(name).GetAxis();
+        }
+        public void SetToggle(string name, bool value)
+        {
+            values.GetValue(name).SetToggle(value);
+        }
+        public bool GetToggle(string name)
+        {
+            return values.GetValue(name).GetToggle();
+        }
+        public void SetDirection(string name, Vector3 value)
+        {
+            values.GetValue(name).SetDirection(value);
+        }
+        public Vector3 GetDirection(string name)
+        {
+            return values.GetValue(name).GetDirection();
+        }
+        public void SetPoint(string name, Vector3 value)
+        {
+            values.GetValue(name).SetPoint(value);
+        }
+        public Vector3 GetPoint(string name)
+        {
+            return values.GetValue(name).GetPoint();
+        }
+        public void SetOrientation(string name, Quaternion value)
+        {
+            values.GetValue(name).SetOrientation(value);
+        }
+        public Quaternion GetOrientation(string name)
+        {
+            return values.GetValue(name).GetOrientation();
+        }
+    }
+}
