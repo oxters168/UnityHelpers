e689fd45f26b8974861b375abd0b91641db49513
Oxters <oxters168@gmail.com>
2021-01-26 13:52:18 -0500

    Fixed some issues with the orientation fixer and floatation mechanism of the PodPhysics. Added a few different input parameters that allow for the control of PodPhysics.

--- Physics/Vehicles/Pod/PodPhysics.cs
+++ Physics/Vehicles/Pod/PodPhysics.cs
@@ -7,29 +7,139 @@ namespace UnityHelpers
         private Rigidbody _podBody;
         private Rigidbody PodBody { get { if (_podBody == null) _podBody = GetComponentInChildren<Rigidbody>(); return _podBody; } }
 
-        [Tooltip("The minimum distance the pod keeps itself floating above the ground (in meters)")]
+        public Vector2 strafeStick;
+        public float brake;
+        public float rotate;
+        public float fly;
+
+        [Space(10), Tooltip("The speed of the speed (in m/s^2)")]
+        public float acceleration = 0.2f;
+        [Tooltip("The fastest speed the vehicle can achieve (in m/s)")]
+        public float maxSpeed = 20;
+
+        [Tooltip("The speed of the fly speed (in m/s^2)")]
+        public float flyAcceleration = 1;
+        [Tooltip("The fastest the vehicle can go up")]
+        public float maxFlySpeed = 10;
+
+        [Tooltip("The speed of the rotational speed (in deg/s^2)")]
+        public float rotAcceleration = 4;
+        [Tooltip("The fastest the vehicle can rotate (in deg/s)")]
+        public float maxRotSpeed = 100;
+
+        [Space(10), Tooltip("The max force applied to stop the vehicle (in newtons")]
+        public float maxBrakeForce = 16000;
+
+        [Space(10), Tooltip("The minimum distance the pod keeps itself floating above the ground (in meters)")]
         public float minGroundDistance = 1;
 
-        [Space(10), Tooltip("The maximum force the pod can apply to keep itself floating above the ground (in newtons)")]
-        public float maxFloatingForce = float.MaxValue;
-        [Tooltip("The maximum torque the pod can apply to fix its orientation (in newton meters)")]
+        [Space(10), Tooltip("How fast the force that floats the pod builds up (in newtons per fixed update)")]
+        public float floatingForceSpeed = 8600;
+        [Tooltip("How much distance beyond the minimum ground height before anti-gravity wears off")]
+        public float antigravityFalloffDistance = 20;
+        public AnimationCurve antigravityFalloffCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+        [Space(10), Tooltip("The maximum torque the pod can apply to fix its orientation (in newton meters)")]
         public float maxCorrectionTorque = float.MaxValue;
 
         [Space(10), Tooltip("The layer(s) to be raycasted when looking for the ground")]
         public LayerMask groundMask = ~0;
 
+        public float prevFloatingForce = 0;
+        public float floatingForce, antigravityForce;
+        public float currentRotSpeed;
+
         void FixedUpdate()
         {
-            //Todo: add speed for planar movement and flight movement and apply force based on input
-            Vector3 floatingForce = CalculateFloatingForce();
-            PodBody.AddForce(floatingForce, ForceMode.Force);
+            ApplyFloatation();
+            ApplyOrientator();
 
-            //Todo: break the correcting torque down to the x-axis and z-axis and take input for the y-axis
-            Vector3 torque = PodBody.CalculateRequiredTorqueForRotation(Quaternion.identity, Time.fixedDeltaTime, maxCorrectionTorque);
-            PodBody.AddTorque(torque, ForceMode.Force);
+            ApplyInputFly();
+            ApplyInputStrafe();
+            ApplyInputBrake();
+            ApplyInputRotate();
         }
 
-        private Vector3 CalculateFloatingForce()
+        private void ApplyInputFly()
+        {
+            float currentVelocity = PodBody.velocity.y;
+            float nextVelocity = currentVelocity + flyAcceleration * fly;
+            float flyForce = PodBody.CalculateRequiredForceForSpeed(Vector3.up * nextVelocity, 0.02f, true).y;
+            PodBody.AddForce(Vector3.up * flyForce, ForceMode.Force);
+        }
+        private void ApplyInputStrafe()
+        {
+            if (!strafeStick.IsZero())
+            {
+                var currentVelocity = new Vector3(PodBody.velocity.x, 0, PodBody.velocity.z);
+                var orthoForward = transform.forward.Planar(Vector3.up);
+                var currentPushDirection = Quaternion.AngleAxis(Vector2.up.GetClockwiseAngle(strafeStick.normalized), Vector3.up) * orthoForward;
+                float currentSpeed = currentVelocity.magnitude * currentVelocity.normalized.PercentDirection(currentPushDirection);
+                float percentPower = strafeStick.magnitude;
+                float inputAcceleration = percentPower * acceleration;
+                float desiredSpeed = Mathf.Clamp(currentSpeed + inputAcceleration, -maxSpeed, maxSpeed);
+                Vector3 appliedForce = PhysicsHelpers.CalculateRequiredForceForSpeed(PodBody.mass, currentSpeed, desiredSpeed) * currentPushDirection;
+                appliedForce = new Vector3(appliedForce.x, 0, appliedForce.z);
+                PodBody.AddForce(appliedForce, ForceMode.Force);
+            }
+        }
+        private void ApplyInputBrake()
+        {
+            if (brake > float.Epsilon)
+            {
+                Vector3 brakeForce = PodBody.CalculateRequiredForceForSpeed(Vector3.zero, 0.02f, false, Mathf.Clamp01(brake) * maxBrakeForce);
+                PodBody.AddForce(brakeForce, ForceMode.Force);
+            }
+        }
+        private void ApplyInputRotate()
+        {
+            bool isAttemptingToRotate = Mathf.Abs(rotate) > float.Epsilon;
+            if (isAttemptingToRotate)
+            {
+                currentRotSpeed = (PodBody.angularVelocity.y * 180f) / Mathf.PI;
+                currentRotSpeed = Mathf.Clamp(currentRotSpeed + rotAcceleration * Mathf.Clamp(rotate, -1, 1), -maxRotSpeed, maxRotSpeed);
+            }
+            else if (Mathf.Abs(currentRotSpeed) > float.Epsilon)
+            {
+                //If there is no input but there is still some rotational speed, then decelerate to zero speed
+                float rotDirection = Mathf.Sign(currentRotSpeed);
+                if (Mathf.Abs(currentRotSpeed) > rotAcceleration)
+                    currentRotSpeed += rotAcceleration * -rotDirection;
+                else
+                    currentRotSpeed = 0;
+            }
+
+            //If the current rot speed is not zero then apply torque on the ship
+            if (Mathf.Abs(currentRotSpeed) > float.Epsilon)
+            {
+                Quaternion resultRotation = PodBody.rotation * Quaternion.AngleAxis(currentRotSpeed * Time.fixedDeltaTime, Vector3.up);
+                Vector3 torque = Vector3.up * PodBody.CalculateRequiredTorqueForRotation(resultRotation, Time.fixedDeltaTime).y;
+                PodBody.AddTorque(torque, ForceMode.Force);
+            }
+        }
+
+        private void ApplyFloatation()
+        {
+            float expectedFloatingForce = CalculateFloatingForce();
+            float deltaFloatingForce = expectedFloatingForce - prevFloatingForce;
+            float currentFloatingForce = prevFloatingForce;
+            //If there is a measurable change in force
+            if (Mathf.Abs(deltaFloatingForce) > float.Epsilon)
+            {
+                //If force is increasing in magnitude
+                // if ((currentFloatingForce > float.Epsilon && Mathf.Sign(deltaFloatingForce) > 0 || currentFloatingForce < float.Epsilon && Mathf.Sign(deltaFloatingForce) < 0))
+                    currentFloatingForce += Mathf.Sign(deltaFloatingForce) * Mathf.Min(Mathf.Abs(deltaFloatingForce), floatingForceSpeed);
+                // else
+                //     currentFloatingForce = expectedFloatingForce;
+            }
+
+            if (Mathf.Abs(fly) > float.Epsilon)
+                currentFloatingForce = 0;
+
+            PodBody.AddForce(Vector3.up * currentFloatingForce, ForceMode.Force);
+            prevFloatingForce = currentFloatingForce;
+        }
+        private float CalculateFloatingForce()
         {
             var vehicleBounds = transform.GetTotalBounds(Space.World);
             float groundCastDistance = vehicleBounds.extents.y + minGroundDistance * 5;
@@ -40,14 +150,35 @@ namespace UnityHelpers
                 groundDistance = hitInfo.distance - vehicleBounds.extents.y;
             Debug.DrawRay(vehicleBounds.center + (rayHitGround ? Vector3.down * vehicleBounds.extents.y : Vector3.zero), Vector3.down * (rayHitGround ? groundDistance : groundCastDistance), rayHitGround ? Color.green : Color.red);
 
-            float upSpeed = 0;
-            Vector3 floatingForce = Vector3.zero;
-            if (groundDistance < minGroundDistance)
+            float groundOffset = minGroundDistance - groundDistance;
+
+            float antigravityMultiplier = 1;
+            if (groundOffset < -float.Epsilon)
+                antigravityMultiplier = antigravityFalloffCurve.Evaluate(Mathf.Max(antigravityFalloffDistance - Mathf.Abs(groundOffset), 0) / antigravityFalloffDistance);
+            antigravityForce = PodBody.CalculateAntiGravityForce().y * antigravityMultiplier;
+
+            floatingForce = 0;
+            if (groundDistance < float.MaxValue)
             {
-                upSpeed = (minGroundDistance - groundDistance);
-                floatingForce = Vector3.up * PodBody.CalculateRequiredForceForSpeed(Vector3.up * upSpeed, Time.fixedDeltaTime, true, maxFloatingForce).y;
+                float currentVerticalForce = PodBody.mass * PodBody.velocity.y / Time.fixedDeltaTime;
+                floatingForce = antigravityMultiplier * PodBody.CalculateRequiredForceForSpeed(new Vector3(0, groundOffset, 0), Time.fixedDeltaTime).y - currentVerticalForce;
             }
-            return floatingForce;
+
+            return (antigravityForce + floatingForce) - prevFloatingForce;
+        }
+
+        private void ApplyOrientator()
+        {
+            Vector3 correctionTorque = CalculateCorrectionTorque();
+            PodBody.AddTorque(correctionTorque, ForceMode.Force);
+        }
+        private Vector3 CalculateCorrectionTorque()
+        {
+            Vector3 correctionTorque;
+            Quaternion correctedOrientation = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up);
+            correctionTorque = PodBody.CalculateRequiredTorqueForRotation(correctedOrientation, Time.fixedDeltaTime, maxCorrectionTorque);
+            correctionTorque = new Vector3(correctionTorque.x, 0, correctionTorque.z);
+            return correctionTorque;
         }
     }
 }
